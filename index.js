97da7ef7c0e7ff99f00219e2150a89b063f3571a
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-09-09 16:18:56 -0700

    created index.html, style.css files & included index into index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
 const teamArray = [];
 var choice = '';
-const html = ``
 
 class Request {
 
@@ -25,6 +25,48 @@ class Request {
             .then(function(data) {
 
                 if (data.choice === 'No More Employees To Add') {
+
+                    console.log(Employee);
+
+                    var html = `<!DOCTYPE html>
+
+                        <html lang="en">
+
+                        <head>
+
+                            <meta charset="UTF-8">
+
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                            <link rel="stylesheet" href="./style.css">
+
+                            <title>Team Profile</title>
+
+                        </head>
+
+                        <body>
+                            <header>
+                                <h1>MY TEAM</h1>
+                            </header>
+
+                            <div class="card">
+                                <div class="heading">
+                                    <h2 class="name">${Engineer.name}</h2>
+                                    <h3 class="role">${data.role}</h3>
+                                </div>
+                                <hr>
+                                <div class="content">
+                                    <p class="id">ID : ${Engineer.id}</p>
+                                    <p class="email">Email : ${Engineer.email}</p>
+                                    <p class="ogs">Github : ${Engineer.github}</p>
+                                </div>
+                            </div>
+
+                            <script src="../index.js"></script>
+                        </body>
+                        </html>`
 
                     fs.writeFile('index.html', html, (err) =>
                         err ? console.log(err) : console.log('Success!')
@@ -63,6 +105,8 @@ class Request {
             ])
             .then(function(data) {
 
+                var employeeArray = [data.name, data.id, data.email];
+            
                 if (choice === 'Manager') {
 
                     inquirer
@@ -75,7 +119,7 @@ class Request {
                         ])
                         .then(function(data) {
                             
-                            const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+                            const manager = new Manager(...employeeArray, data.officeNumber);
                             teamArray.push(manager);
                             request.choicePrompt();
                         })
@@ -92,7 +136,8 @@ class Request {
                         ])
                         .then(function(data) {
                             
-                            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+                            const engineer = new Engineer(...employeeArray, data.github);
+                            console.log(engineer);
                             teamArray.push(engineer);
                             request.choicePrompt();
                         })
@@ -109,7 +154,7 @@ class Request {
                         ])
                         .then(function(data) {
                             
-                            const intern = new Intern(data.name, data.id, data.email, data.github);
+                            const intern = new Intern(...employeeArray, data.github);
                             teamArray.push(intern);
                             request.choicePrompt();
                         })
